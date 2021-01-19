ba335e5c30aae31f1d9a00e247573cc945023349
guilherme gehling <guilherme.gehling@calcard.com.br>
2021-01-19 19:25:06 -0300

    Tratamento de erros de gestão de sessão Vinculo entre as tabelas

--- src/main/java/com/guigehling/voting/enumeration/ErrorCodeEnum.java
+++ src/main/java/com/guigehling/voting/enumeration/ErrorCodeEnum.java
@@ -9,7 +9,10 @@ public enum ErrorCodeEnum {
 
     ERROR_VOTE_SESSION_CLOSED("error.vote.session.closed"),
     ERROR_VOTE_USER_VOTED("error.vote.user.has.voted"),
-    ERROR_VOTE_USER_UNABLE("error.vote.user.unable");
+    ERROR_VOTE_USER_UNABLE("error.vote.user.unable"),
+
+    ERROR_SESSION_OPEN("error.session.open"),
+    ERROR_SESSION_CLOSE("error.session.close");
 
     private final String messageKey;
 }
