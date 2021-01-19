3e17ddfd01617f9f2d4a538430d7ccc1fc5932f5
guilherme gehling <guilherme.gehling@calcard.com.br>
2021-01-18 22:44:00 -0300

    Remoção de classe desnecessaria do async Remover propriedades desnecessarias

--- src/main/java/com/guigehling/voting/Application.java
+++ src/main/java/com/guigehling/voting/Application.java
@@ -3,13 +3,15 @@ package com.guigehling.voting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @EnableFeignClients(basePackages = {"com.guigehling.voting.integration"})
 @SpringBootApplication
-public class VotingApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(VotingApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
