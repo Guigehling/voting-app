1b6f02fb5f94de30daf1d8947a9ec0ea73c94198
guilherme gehling <guilherme.gehling@calcard.com.br>
2021-01-18 21:35:12 -0300

    Realizada a integração com o serviço de usuarios Readequação do sistema de enfileiramento de mensagens, tornando o mesmo mais generico Atualização do POM para suportar adequadamente o OpenFeing

--- src/main/java/com/guigehling/voting/VotingApplication.java
+++ src/main/java/com/guigehling/voting/VotingApplication.java
@@ -2,12 +2,14 @@ package com.guigehling.voting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = {"com.guigehling.voting.integration"})
 @SpringBootApplication
 public class VotingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VotingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VotingApplication.class, args);
+    }
 
 }
